d16ecd50b4a814f7bf3418e2522cac3b9d5c9b4d
eduard <eduard_reich0@proton.me>
2025-11-13 18:43:35 +0300

    refactor migrations a bit

--- migrations/20251112212929_table_users.sql
+++ migrations/20251112212929_table_users.sql
@@ -4,7 +4,9 @@ CREATE TABLE users
 (
     id        uuid PRIMARY KEY NOT NULL,
     name      text             NOT NULL,
-    is_active bool             NOT NULL
+    is_active bool             NOT NULL,
+    team_id   uuid             NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES teams (id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
 
