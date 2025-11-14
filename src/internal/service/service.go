8b5ef04242d068b367ffbbf4ca853398b7163530
eduard <eduard_reich0@proton.me>
2025-11-14 15:53:59 +0300

    describe service struct

--- src/internal/service/service.go
+++ src/internal/service/service.go
@@ -1,4 +1,21 @@
 package service
 
+import "github.com/CargoMan0/avito-tech-task/internal/repository"
+
 type Service struct {
+	pullRequestRepository repository.PullRequestRepository
+	userRepository        repository.UserRepository
+	teamRepository        repository.TeamRepository
+}
+
+func NewService(
+	pullRequestsRepository repository.PullRequestRepository,
+	usersRepository repository.UserRepository,
+	teamsRepository repository.TeamRepository,
+) *Service {
+	return &Service{
+		pullRequestRepository: pullRequestsRepository,
+		userRepository:        usersRepository,
+		teamRepository:        teamsRepository,
+	}
 }
